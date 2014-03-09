ed54102830417cf26f4479bacb1c7d67e6fa0ff2
ryanjadhav <ryan.jadhav@gmail.com>
2014-03-08 23:00:52 -0800

    conditional to fix contact test

--- public/js/main.js
+++ public/js/main.js
@@ -1,16 +1,19 @@
 $(document).ready(function() {
-  var names = new Bloodhound({
-    datumTokenizer: function(d) {
-      return Bloodhound.tokenizers.whitespace(d.name);
-    },
-    queryTokenizer: Bloodhound.tokenizers.whitespace,
-    local: window.cf.dump.friends
-  });
+  var $transactionRecipient = $('#transactionRecipient');
+  if ($transactionRecipient.length) {
+    var names = new Bloodhound({
+      datumTokenizer: function(d) {
+        return Bloodhound.tokenizers.whitespace(d.name);
+      },
+      queryTokenizer: Bloodhound.tokenizers.whitespace,
+      local: window.cf.dump.friends
+    });
 
-  names.initialize();
+    names.initialize();
 
-  $('.typeahead').typeahead(null, {
-    displayKey: 'name',
-    source: names.ttAdapter()
-  });
+    $transactionRecipient.typeahead(null, {
+      displayKey: 'name',
+      source: names.ttAdapter()
+    });
+  }
 });
